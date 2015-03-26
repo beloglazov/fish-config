c83d0ea96ec68953a1cf915fd58aec558ab59eb4
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-03-26 15:02:44 +1100

    Added a new gl based on fzf, renamed the old gl to gll

--- functions/gl.fish
+++ functions/gl.fish
@@ -1,4 +1,13 @@
 function gl
-    git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --abbrev-commit $argv
+    while git log --graph --abbrev-commit \
+            --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %s %Cgreen%cr %C(bold blue)%an%Creset' \
+            | fzf --ansi --no-sort --reverse --query="$q" --print-query > /tmp/fzf
+        set q (cat /tmp/fzf | head -1)
+        set sha (cat /tmp/fzf | tail -1 | cut -d ' ' -f 2)
+        if [ -n "$sha" ];
+            git show --color=always $sha | less -R
+        end
+    end
 end
+
 
